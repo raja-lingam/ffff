6d578b6fcf7f499819fda1dd84b7c34bd4cf66cf
raja-lingam <rajalingamsrrp@gmail.com>
2016-07-11 10:24:37 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,x,p,i,j=1;
+printf("\n Enter the n value:");
+scanf("%d",&n);
+printf("\n Enter the pair");
+scanf("%d",&p);
+for(i=0;i<2*n;i++)
+{
+x=2*n-i;
+if(p!=x)
+{
+printf("The pair is %d for days %d is %d\n",p,j++,p);
+}
+}
+getch();
+}
